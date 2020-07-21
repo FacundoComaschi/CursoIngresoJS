532626f01de25b28b6bfaf178c57ed8e6442e31c
Facundo Comaschi <facucomaschi@gmail.com>
2020-07-21 17:39:08 -0300

    practicando

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -14,3 +14,20 @@ function mostrar()
 
 	}
 	}
+// ejercicio del profesor
+	var edad1;
+	var edad2;
+	var edad3;
+	edad1 = 22;
+	edad2 = 33;
+	edad3= 44;
+
+	if (edad1 >= edad2 && edad1 >= edad3) {
+		alert (edad1);
+	} else {
+		if (edad2 >= edad3){
+			alert (edad2);
+		}else {
+			alert (edad3)
+		}
+	}
